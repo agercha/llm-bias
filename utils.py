a319e53e823413d05444e084d130ae8886665489
agerchan <agerchan@andrew.cmu.edu>
2023-10-12 18:43:36 -0400

    bert weights 11

--- utils.py
+++ utils.py
@@ -229,6 +229,7 @@ def get_embedding_matrix(model):
         return model.base_model.embed_in.weight
     elif isinstance(model, BertModel):
         print(dir(model))
+        print(model.encoder.layer)
         print(model.named_parameters())
         print(dict(model.named_parameters())['bert.embeddings.LayerNorm.weight'])
         print(dir(model.base_model))
