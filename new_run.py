0b4e52584eb592aebee51aab4311bbf7089daa32
agerchan <agerchan@andrew.cmu.edu>
2023-10-26 20:32:32 -0400

    new base

---
+++ new_run.py
@@ -0,0 +1,73 @@
+import torch
+import numpy as np
+from run_utils import *
+import gc
+from transformers import (AutoModelForCausalLM, AutoTokenizer)
+from fastchat.model import get_conversation_template
+
+np.random.seed(20)
+torch.manual_seed(20)
+torch.cuda.manual_seed_all(20)
+
+init_prompt = "I like my doctor."
+
+model_path = "/data/anna_gerchanovsky/anna_gerchanovsky/Llama-2-7b-hf"
+
+female_strs = ["She ",
+                "She's",
+                "her ",
+                " she ",
+                " she's ",
+                " her "
+                ]
+
+male_strs = ["He ",
+                "He's",
+                "His ",
+                " he ",
+                " he's ",
+                " his "
+                ]
+
+fail_strs = male_strs
+
+success_strs = female_strs
+
+# load model
+model = AutoModelForCausalLM.from_pretrained(
+            "llama-2",
+            torch_dtype=torch.float16,
+            trust_remote_code=True,
+        ).to("cuda:0").eval()
+
+# load tokenizer
+tokenizer = AutoTokenizer.from_pretrained(
+        'llama-2',
+        trust_remote_code=True,
+        use_fast=False
+    )
+
+tokenizer.pad_token = tokenizer.unk_token
+tokenizer.padding_side = 'left'
+
+# load template
+conv_template = get_conversation_template('llama-2')
+conv_template.sep2 = conv_template.sep2.strip()
+
+not_allowed_tokens = get_nonascii_toks(tokenizer) 
+
+current_prompt = init_prompt
+
+fail_ids = get_ids(tokenizer, fail_strs)
+success_ids = get_ids(tokenizer, success_strs)
+
+for i in range(1):
+    torch.cuda.empty_cache()
+    prompt_ids = get_ids(tokenizer, current_prompt)
+
+    success_grads = [get_gradients(model, current_prompt, s) for s in success_strs]
+
+    fail_grads = [get_gradients(model, current_prompt, f) for f in fail_strs]
+
+    # grads = success_grads - fail_grads
+    
---
+++ run_utils.py
@@ -0,0 +1,65 @@
+import torch
+
+def get_nonascii_toks(tokenizer, device='cpu'):
+
+    def is_ascii(s):
+        return s.isascii() and s.isprintable()
+
+    ascii_toks = []
+    for i in range(3, tokenizer.vocab_size):
+        if not is_ascii(tokenizer.decode([i])):
+            ascii_toks.append(i)
+    
+    if tokenizer.bos_token_id is not None:
+        ascii_toks.append(tokenizer.bos_token_id)
+    if tokenizer.eos_token_id is not None:
+        ascii_toks.append(tokenizer.eos_token_id)
+    if tokenizer.pad_token_id is not None:
+        ascii_toks.append(tokenizer.pad_token_id)
+    if tokenizer.unk_token_id is not None:
+        ascii_toks.append(tokenizer.unk_token_id)
+    
+    return torch.tensor(ascii_toks, device=device)
+
+def successful(gen_str, success_strs, fail_strs):
+    jailbroken = any([prefix in gen_str for prefix in fail_strs])
+    present = any([prefix in gen_str for prefix in success_strs])
+    print(f'\nPresent: {present} | Jailbroken: {jailbroken}')
+    return present and not jailbroken
+
+def get_ids(tokenizer, vals, device = "cuda:0"):
+        return  [torch.tensor(tokenizer(val).input_ids).to(device) for val in vals]
+
+def get_gradients(model, base_ids, end_ids):
+    embed_weights = model.model.embed_tokens.weight()
+
+    # all_ids = torch.cat((base_ids, end_ids))
+    
+    one_hot = torch.zeros(
+        base_ids.shape[0],
+        embed_weights.shape[0],
+        device=model.device,
+        dtype=embed_weights.dtype
+    )
+    one_hot.scatter_(
+        1, 
+        base_ids.unsqueeze(1),
+        torch.ones(one_hot.shape[0], 1, device=model.device, dtype=embed_weights.dtype)
+    )
+    one_hot.requires_grad_()
+    input_embeds = (one_hot @ embed_weights).unsqueeze(0)
+    
+    end_embeds = model.model.embed_tokens(end_ids.unsqueeze(0)).detach
+    full_embeds = torch.cat(
+        [
+            input_embeds, 
+            end_embeds,
+        ], 
+        dim=1)
+    
+    logits = model(inputs_embeds=full_embeds).logits
+    loss = torch.nn.CrossEntropyLoss()(logits[0,len(base_ids),:], end_ids)
+    
+    loss.backward()
+    
+    return one_hot.grad.clone()
