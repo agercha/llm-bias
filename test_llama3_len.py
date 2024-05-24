c64e2405b7df7ba1040a42b554510932eae38a89
agerchan <agerchan@andrew.cmu.edu>
2024-05-24 11:12:07 -0400

    test one more temp parm

--- test_llama3_len.py
+++ test_llama3_len.py
@@ -1,13 +1,17 @@
 import sys
 import json
 import torch
+import os
 from transformers import (AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM, GemmaForCausalLM, GemmaTokenizer)
 from transformers import pipeline as transformer_pipeline
+import matplotlib.pyplot as plt
+
+
 
 category = sys.argv[1]
 index = sys.argv[2]
 
-filename = f"{category}_{index}.json"
+filename = f"llama3it_test/{category}_{index}.json"
 
 dataset = json.load(open('dataset.json'))
 
@@ -45,11 +49,42 @@ for i in range(1000):
         max_new_tokens=1000,
         eos_token_id=terminators,
         do_sample=True,
-        temperature=0.6,
+        # temperature=0.6,
         top_p=0.9,
     )
 
     responses.append(outputs[0]["generated_text"][len(prompt):])
+    print(responses[-1])
 
-    if i%10 == 0:
-        (open(filename, 'w')).write(json.dumps(responses, indent=4))
+    # if i%10 == 0:
+    (open(filename, 'w')).write(json.dumps(responses, indent=4))
+'''
+
+entries = os.listdir("llama3it_test")
+entries.remove("blank.txt")
+
+str_lens = []
+word_lens = []
+
+for entry in entries:
+    completions = json.load(open(f'llama3it_test/{entry}'))
+
+    for completion in completions:
+        str_lens.append(len(completion))
+        word_lens.append(len(completion.split()))
+
+        
+fig = plt.figure(figsize=(14,8))
+plt.suptitle("Llama3it lengths")
+plt.xlabel("Number of characters")
+plt.ylabel("Number of completions")
+plt.hist(str_lens, bins=32)
+plt.show()
+        
+fig = plt.figure(figsize=(14,8))
+plt.suptitle("Llama3it lengths")
+plt.xlabel("Number of words")
+plt.ylabel("Number of completions")
+plt.hist(word_lens, bins=32)
+plt.show()
+'''
