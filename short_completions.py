72cfb9f72fc7927daf71f234d47b00f6667c5a12
agerchan <agerchan@andrew.cmu.edu>
2024-06-13 13:55:50 -0400

    start for llama3it new

--- short_completions.py
+++ short_completions.py
@@ -101,7 +101,30 @@ def generate(model, modelname, tokenizer, prompt, input_ids, pipeline, gen_confi
         )
         
         return outputs[0]["generated_text"]
-    if modelname == "llama3it":
+    elif modelname == "llama3itnew":
+        messages = [
+            {"role": "user", "content": prompt},
+        ]
+        prompt = pipeline.tokenizer.apply_chat_template(
+                messages, 
+                tokenize=False, 
+                add_generation_prompt=True
+        )
+        terminators = [
+            pipeline.tokenizer.eos_token_id,
+            pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        ]
+        outputs = pipeline(
+            prompt,
+            max_new_tokens=64,
+            eos_token_id=terminators,
+            do_sample=True,
+            # temperature=0.6,
+            temperature=1,
+            top_p=0.9,
+        )
+        return outputs[0]["generated_text"]
+    elif modelname == "llama3it":
         messages = [
             {"role": "user", "content": prompt},
         ]
@@ -203,6 +226,28 @@ def run(modelname, category):
             )
         pipeline = None
     elif modelname == "llama3it":
+        model_path = "/data/anna_gerchanovsky/anna_gerchanovsky/Meta-Llama-3-8B-Instruct"
+        model = LlamaForCausalLM.from_pretrained(
+                model_path,
+                torch_dtype=torch.float16,
+                trust_remote_code=True,
+            ).to("cuda:0").eval()
+        tokenizer = AutoTokenizer.from_pretrained(
+                model_path,
+                trust_remote_code=True,
+                use_fast=False
+            )
+        pipeline = transformer_pipeline(
+            "text-generation",
+            model=model_path,
+            model_kwargs={"torch_dtype": torch.bfloat16},
+            device_map="auto",
+        )
+        # terminators = [
+        #     pipeline.tokenizer.eos_token_id,
+        #     pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+        # ]
+    elif modelname == "llama3itnew":
         model_path = "/data/anna_gerchanovsky/anna_gerchanovsky/Meta-Llama-3-8B-Instruct"
         model = LlamaForCausalLM.from_pretrained(
                 model_path,
@@ -358,15 +403,15 @@ def run(modelname, category):
                         {"role": "user", "content":curr_prompt},
                     ]
                     curr_prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-                # elif modelname == "llama3it":
-                #     messages = [
-                #         {"role": "user", "content": curr_prompt},
-                #     ]
-                #     curr_prompt = pipeline.tokenizer.apply_chat_template(
-                #             messages, 
-                #             tokenize=False, 
-                #             add_generation_prompt=True
-                #     )
+                elif modelname == "llama3itnew":
+                    messages = [
+                        {"role": "user", "content": curr_prompt},
+                    ]
+                    curr_prompt = pipeline.tokenizer.apply_chat_template(
+                            messages, 
+                            tokenize=False, 
+                            add_generation_prompt=True
+                    )
 
                 losses[prompt_ind] = my_loss(model, tokenizer, curr_prompt, target_strs, device)
 
